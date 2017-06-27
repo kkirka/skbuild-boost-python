9f71ff30f50e8cbf068d0ab5216600c411911d2e
Kirill Kuvshinov <kkirka@gmail.com>
2017-06-27 15:08:42 +0300

    Add build tool

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     description="a minimal example package (cpp version)",
     author='The scikit-build team',
     license="MIT",
+    cmake_args=['-DCMAKE_BUILD_TOOL:STRING=make'],
     packages=['hello'],
 )
